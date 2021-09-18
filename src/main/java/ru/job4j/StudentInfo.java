81f666601a27d04fadedbf541b833563153147fd
Andrei.Sevostyanov <Andrei.Sevostyanov@gmail.com>
2021-09-18 19:50:25 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrei Sevostyanov");
+        System.out.println("18.09.2021");
+    }
+}
